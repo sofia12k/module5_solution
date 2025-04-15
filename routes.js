50aafefad27ac70235f9d083b43909fcf329da62
sofia12k <145674708+sofia12k@users.noreply.github.com>
2025-04-15 12:05:17 +0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,22 +1,37 @@
-$stateProvider
-  .state('public', {
-    abstract: true,
-    template: '<ui-view></ui-view>'
-  })
+(function () {
+'use strict';
 
-  .state('public.menu', {
-    url: '/menu',
-    templateUrl: 'menu.html',
-    controller: 'MenuController as menuCtrl'
-  })
+angular.module('public')
+.config(routeConfig);
 
-  .state('public.menuitems', {
-    url: '/menu/{category}',
-    templateUrl: 'menu-items.html',
-    controller: 'MenuItemsController as menuItemsCtrl',
-    resolve: {
-      menuItems: ['MenuService', '$stateParams', function(MenuService, $stateParams) {
-        return MenuService.getMenuItems($stateParams.category);
-      }]
-    }
-  });
+/**
+ * Configures the routes and views
+ */
+routeConfig.$inject = ['$stateProvider'];
+function routeConfig ($stateProvider) {
+  $stateProvider
+    .state('public', {
+      abstract: true,
+      templateUrl: 'src/public/public.html'
+    })
+
+    .state('public.menu', {
+      url: '/menu',
+      templateUrl: 'src/menu/menu.html',
+      controller: 'MenuController as menuCtrl'
+    })
+
+    .state('public.menuitems', {
+      url: '/menu/{category}',
+      templateUrl: 'src/menu/menu-items.template.html',
+      controller: 'MenuItemsController as menuItemsCtrl',
+      resolve: {
+        menuItems: ['MenuService', '$stateParams', function (MenuService, $stateParams) {
+          return MenuService.getMenuItems($stateParams.category);
+        }]
+      }
+    });
+}
+
+})();
+
